5596180ed1ed98b5a1a672a8ff847f5d4d3ff922
Red5d <daniel.colson99@gmail.com>
2016-11-13 20:03:47 -0500

    Fixed $OLDPWD being changed

--- context.bash
+++ context.bash
@@ -1,12 +1,15 @@
 #! /bin/bash
 
 c() {
+  oldpwd="$OLDPWD"
   cd ~/.local/bin/contextcommands
   cmd=$(lastcmd=$(history 2 | head -1 | cut -d ' ' -f 2-); python context.py $1 $lastcmd)
   if [[ $cmd != echo* ]];then
-    echo $cmd
+    echo "$cmd"
   fi
 
+  cd - 1>/dev/null
+  OLDPWD="$oldpwd"
   history -s "$cmd"
   eval "$cmd"
 }
