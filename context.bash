7ef2cc608dce842650a638197f5bb4b1e4e7896b
Red5d <daniel.colson99@gmail.com>
2016-12-08 11:32:57 -0500

    Use awk instead of cut to extract cmd from history

--- context.bash
+++ context.bash
@@ -3,7 +3,7 @@
 c() {
   oldpwd="$OLDPWD"
   cd ~/.local/bin/contextcommands
-  cmd=$(lastcmd=$(history 2 | head -1 | cut -d ' ' -f 2-); python context.py $1 $lastcmd)
+  cmd=$(lastcmd=$(history 2 | head -1 | awk '{$1=""; print $0}'); python context.py $1 $lastcmd)
   if [[ $cmd != echo* ]];then
     echo "$cmd"
   fi
