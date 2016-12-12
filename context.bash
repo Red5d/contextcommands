f2da83f32f761e01ea62fcf23e8ef0a83b793127
Red5d <daniel.colson99@gmail.com>
2016-12-12 12:27:36 -0500

    Better check for no context cmd
    Check for "false" return message from python script and don't add to history if "false" found.

--- context.bash
+++ context.bash
@@ -4,12 +4,16 @@ c() {
   oldpwd="$OLDPWD"
   cd ~/.local/bin/contextcommands
   cmd=$(lastcmd=$(history 2 | head -1 | awk '{$1=""; print $0}'); python context.py $1 $lastcmd)
-  if [[ $cmd != echo* ]];then
+  if [[ "$cmd" != "false" ]];then
     echo "$cmd"
   fi
 
   cd - 1>/dev/null
   OLDPWD="$oldpwd"
-  history -s "$cmd"
-  eval "$cmd"
+  if [ "$cmd" != "false" ];then
+    history -s "$cmd"
+    eval "$cmd"
+  else
+    echo "No context commands found."
+  fi
 }
