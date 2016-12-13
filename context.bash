0859e7dd6f85b58d3e4b46d20be53f2e93f81635
Red5d <daniel.colson99@gmail.com>
2016-12-13 10:13:14 -0500

    Remove cd to contextcommands folder
    The cd is now done in the python script instead to reduce complexity.

--- context.bash
+++ context.bash
@@ -1,15 +1,11 @@
 #! /bin/bash
 
 c() {
-  oldpwd="$OLDPWD"
-  cd ~/.local/bin/contextcommands
-  cmd=$(lastcmd=$(history 2 | head -1 | awk '{$1=""; print $0}'); python context.py $1 $lastcmd)
+  cmd=$(lastcmd=$(history 2 | head -1 | awk '{$1=""; print $0}'); python ~/.local/bin/contextcommands/context.py $1 $lastcmd)
   if [[ "$cmd" != "false" ]];then
     echo "$cmd"
   fi
 
-  cd - 1>/dev/null
-  OLDPWD="$oldpwd"
   if [ "$cmd" != "false" ];then
     history -s "$cmd"
     eval "$cmd"
